4517c957b399af05179ac5ee7e6c6a560da46891
diane <diminibonboulash@gmail.com>
2024-12-10 20:51:26 +0800

    edited

--- src/components/notfound/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-const NotFound = () => {
-  return ( <h3>
-    This page does not exist.
-  </h3> );
-}
- 
-export default NotFound;
--- src/constants/constants.js
+++
@@ -1,3 +0,0 @@
-export const apiURL = "https://kaikai-eight.vercel.app"
-
-export const apiURL2 = "https://kaikai-api-27e7ca2ff396.herokuapp.com"
